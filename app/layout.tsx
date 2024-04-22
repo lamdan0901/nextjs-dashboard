8e47eb78af9b9faf1fb5d894de4dd5a5ebcc006c
lam.ho <lam.ho@sotatek.com>
2024-04-22 18:19:58 +0700

    finalize code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,24 @@
 import '@/app/ui/global.css';
 import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+
+  // Open Graph Metadata: This metadata enhances the way a webpage is represented when shared on social media platforms, providing information such as the title, description, and preview image.
+  // openGraph: {
+  //   title: '',
+  //   description: '',
+  //   images: '',
+  // },
+
+  // icons:"path/to/favicon.ico"
+};
 
 const inter = Inter({ subsets: ['latin'] });
 
